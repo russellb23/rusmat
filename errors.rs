88575a824ceb4f5b473d9a0d5dc11ba6b4ee2d45
russellb23 <bertrussell23@yahoo.in>
2019-07-04 11:36:57 +0530

    err

---
+++ errors.rs
@@ -0,0 +1,22 @@
+// error_chain is causing unused_doc_comment warnings
+#[allow(unused_doc_comments)]
+error_chain! {
+    errors {
+        IndexError(s: &'static str) {
+            description("Index error")
+            display("Index error: {}", s)
+        }
+        DecompositionError(s: String) {
+            description("Decomposition error")
+            display("Decomposition error: {}", s)
+        }
+        SolveError(s: String) {
+            description("Solver error")
+            display("Solver error: {}", s)
+        }
+        CondError(s: String) {
+            description("Condition error")
+            display("Condition error: {}", s)
+        }
+    }
+}
